ac703d6cb323e6a7331e0770bb7c2561cb1418e2
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-02-02 12:05:44 +0100

    add initial BookingSystem test setup and add first test bookRoomShouldReturnTrueWhenRoomIsAvailable

---
+++ src/test/java/com/example/BookingSystemTest.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookingSystemTest {
+
+    @Mock TimeProvider timeProvider;
+    @Mock RoomRepository roomRepository;
+    @Mock NotificationService notificationService;
+
+    @Captor ArgumentCaptor<Booking> bookingCaptor;
+
+    private BookingSystem bookingSystem;
+    private String roomId;
+    private LocalDateTime now;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Room room;
+
+    @BeforeEach
+    void setUp(){
+        bookingSystem = new BookingSystem(timeProvider, roomRepository, notificationService);
+
+        roomId = "R1";
+        now = LocalDateTime.of(2026, 2, 2, 10, 0);
+        start = now.plusHours(1);
+        end = now.plusHours(2);
+
+        room = new Room(roomId, "Conference Room");
+
+        when(timeProvider.getCurrentTime()).thenReturn(now);
+        when(roomRepository.findById(roomId)).thenReturn(Optional.of(room));
+
+    }
+
+    @Test
+    void bookRoomShouldReturnTrueWhenRoomIsAvailable(){
+        boolean result = bookingSystem.bookRoom(roomId, start, end);
+
+        assertThat(result).isTrue();
+    }
+}
